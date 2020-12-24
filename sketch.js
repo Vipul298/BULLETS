42c8a11d8722f5d7c9b8ca15ff71c29c540369c0
Vipul298 <75067122+Vipul298@users.noreply.github.com>
2020-12-24 14:32:49 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -23,12 +23,12 @@ function draw() {
     var damage = 0.5 *weight*speed*speed/(thickness*thickness*thickness);
     if(damage>10)
     {
-       wall.shapecolor = ("green");
+      wall.shapeColor=color(255,0,0);
     }
          
     if(damage<10)
     {
-      wall.shapecolor = ("red");
+     wall.shapeColor=color(0,255,0);
     }
 
   } 
